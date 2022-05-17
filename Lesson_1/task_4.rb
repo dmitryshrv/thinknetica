fb022a372675d7a3b171da772f8d57ea7c3da532
dmitryshrv <105642961+dmitryshrv@users.noreply.github.com>
2022-05-17 19:03:15 +0400

    Lesson 1

---
+++ Lesson_1/task_4.rb
@@ -0,0 +1,17 @@
+puts "Веедите коэффициенты уравнения ax**2 + bx + c = 0: "
+print "Коэффициент a: "
+a = gets.to_f
+print "Коэффициент b: "
+b = gets.to_f
+print "Коэффициент c: "
+c = gets.to_f
+
+d = b**2 - 4*a*c
+
+if d<0
+  print "Корней нет"
+elsif d==0
+    print "В уравнении один корень = #{-b/(2*a)}"
+else
+    print "В уравнении два корня х1=#{(-b+Math.sqrt(d))/(2*a)} х2=#{(-b-Math.sqrt(d))/(2*a)}"
+end
