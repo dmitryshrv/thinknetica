d665b987a4e2d7192c099a6862c15ed863f0e33e
dmitryshrv <105642961+dmitryshrv@users.noreply.github.com>
2022-05-18 12:43:51 +0400

    spaces and crlf Lesson1

--- Lesson_1/task_1.rb
+++ Lesson_1/task_1.rb
@@ -1,8 +1,10 @@
-puts "Как вас зовут? "
+puts "Как вас зовут?"
 name = gets.chomp
-puts "Введите ваш рост в сантиметрах: "
+
+puts "Введите ваш рост в сантиметрах:"
 height = gets.chomp.to_i
-ideal_weight = (height - 110)*1.15
+
+ideal_weight = (height - 110) * 1.15
 
 if ideal_weight < 0
   puts "#{name}, ваш вес уже оптимальный"
--- Lesson_1/task_3.rb
+++ Lesson_1/task_3.rb
@@ -1,6 +1,6 @@
 
-def is_triangle(a,b,c) #существует ли такой треугольник
-  (a+b)>c && (a+c)>b && (b+c)>a    
+def is_triangle(a, b, c) #существует ли такой треугольник
+  (a + b) > c && (a + c) > b && (b + c) > a    
 end
 
 puts "Введите три стороны треугольника"
@@ -13,9 +13,9 @@ b = gets.to_f
 print "3 сторона = "
 c = gets.to_f
 
-sides = [a,b,c].sort
+sides = [a, b, c].sort
 
-if is_triangle(a,b,c)
+if is_triangle(a, b, c)
 
   if sides[2]**2 == sides[1]**2 + sides[0]**2
     print "Треугольник прямоугольный"
@@ -27,5 +27,6 @@ if is_triangle(a,b,c)
     print "Треугольник не является прямоугольным, равнобедренным, равносторонним"
   end
   
-else print "Треугольника с такими сторонами не существует"
+else 
+  print "Треугольника с такими сторонами не существует"
 end
